bd41e6bcf22299f679c2085609282687e4894820
Laotshi <laotshi@gmail.com>
2015-11-20 01:52:07 -0600

    Documentation of the document.

--- app/src/main/java/com/example/gus/places/fragment/DetailFragment.java
+++ app/src/main/java/com/example/gus/places/fragment/DetailFragment.java
@@ -14,11 +14,7 @@ import com.example.gus.places.R;
 import com.squareup.picasso.Picasso;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link DetailFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * It is the fragment for open the detail of the item
  */
 public class DetailFragment extends Fragment {
 
